1d0ce6da080a8ccfb3fd29ecdfeb0c63fce188fe
Yunus Emre KORKMAZ <47673647+ynsmrkrkmz@users.noreply.github.com>
2024-05-17 22:32:04 +0300

    fix: cloud run service environment bug fixed

--- infoshare-backend/build.sh
+++ infoshare-backend/build.sh
@@ -4,6 +4,6 @@
 IMAGE_NAME="us-central1-docker.pkg.dev/infoshare-423320/infoshare/infoshare-backend"
 
 # Build the Docker image using the Dockerfile
-docker build --build-arg SPRING_PROFILE=prod -t $IMAGE_NAME . --no-cache
+docker build -t $IMAGE_NAME . --no-cache
 
 docker push $IMAGE_NAME
